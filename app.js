135f9211b4e49df52a15997d33810a5a07f6ff4c
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-04 18:59:09 +0200

    add function to calculate match and beginning of the logic

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ calculateBtn.addEventListener('click', triggerModalo);
 
 function triggerModalo() {
   // ADD A SPINNER TO INDICATE THAT IT IS LOADING 
+  calculateMatch();
   const modalo = document.querySelector('.modalo-container');
   modalo.style.display = 'block';
 }
@@ -18,4 +19,29 @@ modaloNewBtn.addEventListener('click', closeModalo);
 function closeModalo() {
   const modalo = document.querySelector('.modalo-container');
   modalo.style.display = 'none';
+}
+
+
+
+function calculateMatch() {
+  const name1 = document.querySelector('#name1');
+  const name2 = document.querySelector('#name2');
+  const scoreInput = document.querySelector('.comp-score');
+  const messageInput = document.querySelector('.comp-message');
+  const array1 = ['This will go great', 'This will be amazing']
+
+  const score = Math.floor((Math.random() + 0.5) * (Math.random() + 70));
+
+  
+  
+  if ((name1.value === 'Rivon' || name1.value === 'Michael') && (name2.value === 'Rivon' || name2.value === 'Michael')) {
+    scoreInput.innerText = `${score}%`;
+    messageInput.innerText = 'message';
+
+  } else if (name1.value == 'Rivon' || name2.value == 'Rivon') {
+    scoreInput.innerText = '17%';
+    messageInput.innerText = 'This is not working';
+    
+  }
+  
 }
