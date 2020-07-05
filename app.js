342a58311e7dfe2b9a1998f6f180cd4fb2496ede
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-05 07:44:22 +0200

    build out the rest of the logic for calculating the match

--- app.js
+++ app.js
@@ -28,20 +28,34 @@ function calculateMatch() {
   const name2 = document.querySelector('#name2');
   const scoreInput = document.querySelector('.comp-score');
   const messageInput = document.querySelector('.comp-message');
-  const array1 = ['This will go great', 'This will be amazing']
+  const array1 = ['This will go great', 'This will be amazing', 'This will be the best thing ever']
+  const array2 = ['This is an okay match', 'Not the best, go for Michael instead', 'Bad Match']
 
-  const score = Math.floor((Math.random() + 0.5) * (Math.random() + 70));
+  const score = Math.floor((Math.random() * 15) + 85);
+  const score2 = Math.floor((Math.random() * 10) + 35);
+  const score3 = Math.floor((Math.random() * 100));
 
   
+
   
   if ((name1.value === 'Rivon' || name1.value === 'Michael') && (name2.value === 'Rivon' || name2.value === 'Michael')) {
     scoreInput.innerText = `${score}%`;
-    messageInput.innerText = 'message';
+    messageInput.innerText = array1[Math.floor(Math.random() * 3)];
 
   } else if (name1.value == 'Rivon' || name2.value == 'Rivon') {
-    scoreInput.innerText = '17%';
-    messageInput.innerText = 'This is not working';
-    
+    scoreInput.innerText = `${score2}%`;
+    messageInput.innerText = array2[Math.floor(Math.random() * 3)];
+  } else {
+    scoreInput.innerText = `${score3}%`
+    if (score3 > 80) {
+      
+      messageInput.innerText = 'Great match'
+    } else if (score3 < 80 && score3 > 50) {
+      messageInput.innerText = 'Average match'
+
+    } else {
+      messageInput.innerText = 'Bad match'
+    }
   }
   
 }
