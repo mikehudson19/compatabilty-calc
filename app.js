92d90214b93c4b2924c9e51cb7352cd2f862882e
Admin <admin@Michaels-MacBook-Pro.local>
2020-07-05 13:42:33 +0200

    Small style changes and adding new match comments.

--- app.js
+++ app.js
@@ -3,51 +3,55 @@ const modaloAgainBtn = document.getElementById('calcAgain');
 const modaloNewBtn = document.getElementById('calcNew');
 
 
+
 calculateBtn.addEventListener('click', triggerModalo);
-
-function triggerModalo() {
-  const loader = document.querySelector('.loader');
-  loader.style.display = 'block';
-  calculateMatch();
-  setTimeout(() => {
-    const modalo = document.querySelector('.modalo-container');
-    modalo.style.display = 'block';
-  }, 500)
-}
-
 modaloAgainBtn.addEventListener('click', triggerModalo);
 modaloNewBtn.addEventListener('click', closeModalo);
 
+
+function triggerModalo() {
+  const loader = document.querySelector('.loader');
+  const modalo = document.querySelector('.modalo-container');
+  modalo.style.display = 'none';
+  loader.style.display = 'flex';
+  calculateMatch();
+  setTimeout(() => {
+    loader.style.display = 'none';
+    modalo.style.display = 'flex';
+  }, 3500)
+}
+
 function closeModalo() {
+  const name1 = document.querySelector('#name1');
+  const name2 = document.querySelector('#name2');
   const modalo = document.querySelector('.modalo-container');
   modalo.style.display = 'none';
   const loader = document.querySelector('.loader');
   loader.style.display = 'none';
-
+  name1.value = '';
+  name2.value = '';
 }
-
-
 
 function calculateMatch() {
   const name1 = document.querySelector('#name1');
   const name2 = document.querySelector('#name2');
   const scoreInput = document.querySelector('.comp-score');
   const messageInput = document.querySelector('.comp-message');
-  const array1 = ['This will go great', 'This will be amazing', 'This will be the best thing ever']
+  const array1 = ['You two go together like rice cakes and peanut butter (plus syrup, banana and cinnamon).', 'The universe brought you two together for a reason.', 'She is the strawberry to his chocolate.', 'Soulmates don\'t exist, but if they did, you two can stop looking.']
   const array2 = ['This is an okay match', 'Not the best, go for Michael instead', 'Bad Match']
 
   const score = Math.floor((Math.random() * 15) + 85);
   const score2 = Math.floor((Math.random() * 10) + 35);
   const score3 = Math.floor((Math.random() * 100));
 
+  const title = document.querySelector('.modalo-title');
+  title.innerText = `The compatability score for ${name1.value} and ${name2.value} is:`
   
-
-  
-  if ((name1.value === 'Rivon' || name1.value === 'Michael') && (name2.value === 'Rivon' || name2.value === 'Michael')) {
+  if ((name1.value === 'Rivon Pretorius' || name1.value === 'Michael Hudson') && (name2.value === 'Rivon Pretorius' || name2.value === 'Michael Hudson')) {
     scoreInput.innerText = `${score}%`;
     messageInput.innerText = array1[Math.floor(Math.random() * 3)];
 
-  } else if (name1.value == 'Rivon' || name2.value == 'Rivon') {
+  } else if (name1.value == 'Rivon Pretorius' || name2.value == 'Rivon Pretorius') {
     scoreInput.innerText = `${score2}%`;
     messageInput.innerText = array2[Math.floor(Math.random() * 3)];
   } else {
@@ -56,10 +60,10 @@ function calculateMatch() {
       
       messageInput.innerText = 'Great match'
     } else if (score3 < 80 && score3 > 50) {
-      messageInput.innerText = 'Average match'
+      messageInput.innerText = 'Average match.'
 
     } else {
-      messageInput.innerText = 'Bad match'
+      messageInput.innerText = 'Bad match.'
     }
   }
   
